79812104da74d85ce2f47b6dc16568b4be04dd50
emanatif270 <emanatif270@gmail.com>
2023-09-14 14:50:14 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ *only using _putchar twice
+ *
+ * Return: always 0 success
+ */
+
+void print_numbers(void)
+{
+	int mun = 0;
+
+	do {
+		_putchar(num + 48);
+		num++
+	} while (num >= 0 && num <= 9);
+	-putchar('\n');
+}
