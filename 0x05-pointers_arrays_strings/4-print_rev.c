6aa1ad64888fd0a378b8e68139a78058728771c3
emanatif270 <emanatif270@gmail.com>
2023-09-18 13:26:33 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reverse string
+ * @s: prints to the string prints
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar('\n');
+}
