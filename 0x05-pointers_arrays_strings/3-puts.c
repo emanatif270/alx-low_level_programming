866c2578ce49b1011dfb6aeafcca7279ac4a4440
emanatif270 <emanatif270@gmail.com>
2023-09-18 13:07:38 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts - pints a string
+ *
+ * @str: string to print
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar (*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
