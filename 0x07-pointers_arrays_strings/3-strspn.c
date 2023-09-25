750e11bd8139bf25e4651392988770706448b141
emanatif270 <emanatif270@gmail.com>
2023-09-25 14:21:10 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpn - grts the lenghth of a prefix substring
+ *
+ * @s: string
+ * @accept: bytes
+ * Return: unsigned int
+ */
+unsigned int _strpn(char *s, char *accept)
+{
+	unsigned int it, jt;
+
+	for (it = 0; s[it] != '\0'; it++)
+	{
+		for (jt = 0; accept[jt] != s[it]; jt++)
+		{
+			if (accept[jt] == '\0')
+				return (it);
+		}
+	}
+	return (it);
+}
