544a2b9298f1705687b634c117597d4e1ed46d1a
emanatif270 <emanatif270@gmail.com>
2023-09-25 17:28:04 -0700

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,18 @@
+#include "mian.h"
+#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char *s = "hello , world";
+	char *f = "world";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
+}
