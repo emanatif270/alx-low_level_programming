f4951996f49f983939614a688fd46fdd4aa6ac19
emanatif270 <emanatif270@gmail.com>
2023-10-04 13:39:03 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,22 +1,21 @@
-#include "main.h"
+#include "mian.h"
 
 /**
- * *create_array - creates an array of chars
+ * *create_array - creates an array of char
  * @size: size of the array
  * @c: char
- * Return: pointer to the array
+ * Return: pointer
  */
 
-char *craete_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
 
 	if (size == 0 || n == 0)
 		return (0);
-
+	
 	while (size--)
 		n[size] = c;
 
 	return (n);
 }
-
