436175b17359df49ebe94e862aa8b542edc100bd
emanatif270 <emanatif270@gmail.com>
2023-10-10 17:30:20 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - initialize
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: just a lone
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
