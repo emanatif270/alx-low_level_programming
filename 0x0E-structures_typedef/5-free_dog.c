db48a69d5aaa2c702801dddf708ffbefa8b523f6
emanatif270 <emanatif270@gmail.com>
2023-10-10 19:45:18 -0700

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,17 +5,16 @@
  * free_dog - free dog
  * dog_t - typedef for struct dog
  * @d: pointer to the struct
- * 
  * Retrun: none
  */
 
 void free_dog(dog_t *d)
 {
-	typedef struct dog dog_t;
+
 	if (d)
 	{
 		free(d->name);
-		free(d->onwer);
+		free(d->owner);
 		free(d);
 	}
 }
