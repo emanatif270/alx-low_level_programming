075f70f25c24219940caf6db2e85b5d2133a765f
emanatif 270 <emanatif270@gmail.com>
2023-10-18 06:59:01 -0700

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the min num od coins
+ * @argc: num of arg
+ * @argv: array of arg
+ * Return: return 1 if there is a error
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
