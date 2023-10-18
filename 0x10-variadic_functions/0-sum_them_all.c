09208c5f9f760f8ae2b90794c231e40426f3b9bc
emanatif 270 <emanatif270@gmail.com>
2023-10-18 07:14:40 -0700

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function that returns the sum
+ * @n: the num of items
+ * Return: return the sum od all
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int count;
+	int sum;
+
+	sum = 0;
+	va_start(ap, n);
+
+	for (count = 0; count < n; count++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (sum);
+}
+
